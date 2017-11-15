b4bbaa6fb3f74ed8071260cc09825a9b1468601d
Miroslav Pashaliski <miroslav.pashaliski@codeit.mk>
2017-11-15 16:48:47 +0100

    DashboardUI and DashboardLayout

---
+++ src/main/java/com/techhub/techhub/DashboadLayout.java
@@ -0,0 +1,11 @@
+package com.techhub.techhub;
+
+import com.vaadin.spring.annotation.SpringComponent;
+import com.vaadin.ui.VerticalLayout;
+
+/**
+ * Created by Miroslav Pashaliski on 11/15/2017.
+ */
+@SpringComponent
+public class DashboadLayout extends VerticalLayout{
+}
--- src/main/java/com/techhub/techhub/DashboardUI.java
+++ src/main/java/com/techhub/techhub/DashboardUI.java
@@ -2,21 +2,27 @@ package com.techhub.techhub;
 
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Created by Miroslav Pashaliski on 11/15/2017.
  */
 @SpringUI
 public class DashboardUI extends UI {
+
     private VerticalLayout root;
+
+    @Autowired
+    DashboadLayout dashboadLayout;
 
     @Override
     protected void init(VaadinRequest request){
         setUpLayout();
         addHeader();
+        addForm();
+        addDashboardList();
+        addDeleteButton();
     }
 
     private void setUpLayout() {
@@ -26,5 +32,24 @@ public class DashboardUI extends UI {
 
     private void addHeader() {
         root.addComponents(new Label("Dashboard"));
+    }
+
+    private void addForm(){
+        HorizontalLayout formLayout = new HorizontalLayout();
+
+        TextField task = new TextField();
+        Button add = new Button("Add");
+
+        formLayout.addComponents(task, add);
+
+        root.addComponent(formLayout);
+    }
+
+    private void addDashboardList(){
+        root.addComponent(dashboadLayout);
+    }
+
+    private void addDeleteButton(){
+        root.addComponent(new Button("Delete complete"));
     }
 }
