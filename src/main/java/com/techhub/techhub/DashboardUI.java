882f64c08113a9c2f8ae1809d7f854909e719d16
Miroslav Pashaliski <miroslav.pashaliski@codeit.mk>
2017-11-15 16:55:26 +0100

    small changes

--- src/main/java/com/techhub/techhub/DashboardUI.java
+++ src/main/java/com/techhub/techhub/DashboardUI.java
@@ -1,8 +1,10 @@
 package com.techhub.techhub;
 
+import com.vaadin.icons.VaadinIcons;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.*;
+import com.vaadin.ui.themes.ValoTheme;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
@@ -27,25 +29,33 @@ public class DashboardUI extends UI {
 
     private void setUpLayout() {
         root = new VerticalLayout();
+        root.setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
         setContent(root);
     }
 
     private void addHeader() {
-        root.addComponents(new Label("Dashboard"));
+        Label header = new Label("Dashboard");
+        header.addStyleName(ValoTheme.LABEL_H1);
+        root.addComponents(header);
     }
 
     private void addForm(){
         HorizontalLayout formLayout = new HorizontalLayout();
+        formLayout.setWidth("80%");
 
         TextField task = new TextField();
-        Button add = new Button("Add");
+        Button add = new Button("");
+        add.addStyleName(ValoTheme.BUTTON_PRIMARY);
+        add.setIcon(VaadinIcons.PLUS);
 
-        formLayout.addComponents(task, add);
+        formLayout.addComponentsAndExpand(task);
+        formLayout.addComponents(add);
 
         root.addComponent(formLayout);
     }
 
     private void addDashboardList(){
+        dashboadLayout.setWidth("80%");
         root.addComponent(dashboadLayout);
     }
 
